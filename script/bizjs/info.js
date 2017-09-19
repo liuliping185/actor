4c717826765041e4147ab68c678a4b84fb7e2e61
liuliping185 <liuliping521>
2017-09-19 09:15:50 +0800

    跳转页面头显示不全修改

---
+++ script/bizjs/info.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('body').height($('body')[0].clientHeight);
+});
--- script/bizjs/personalInfo.js
+++ script/bizjs/personalInfo.js
@@ -15,6 +15,8 @@ $(function(){
             },function(ret){
                 if(ret){
                     $("#loginname").val(data.memberinfo.loginname);
+                    $("#birthday").val(data.memberinfo.birthday);
+                    $("#realname").val(data.memberinfo.realname);
                     $("#phone").val(data.memberinfo.phone);
                     $("#address").val(data.memberinfo.address);
                     $("#alipay").val(data.memberinfo.alipay);
@@ -36,13 +38,63 @@ $(function(){
           })
         }
     });
+
+    var currYear = (new Date()).getFullYear();	
+    var opt={};
+    opt.date = {preset : 'date'};
+    //opt.datetime = { preset : 'datetime', minDate: new Date(2012,3,10,9,22), maxDate: new Date(2014,7,30,15,44), stepMinute: 5  };
+    opt.datetime = {preset : 'datetime'};
+    opt.time = {preset : 'time'};
+    opt.default = {
+      theme: 'android-ics light', //皮肤样式
+          display: 'modal', //显示方式
+          mode: 'scroller', //日期选择模式
+      lang:'zh',
+          startYear:currYear - 10, //开始年份
+          endYear:currYear + 10 //结束年份
+    };
+
+    $("#appDate").val('').scroller('destroy').scroller($.extend(opt['date'], opt['default']));
+      var optDateTime = $.extend(opt['datetime'], opt['default']);
+      var optTime = $.extend(opt['time'], opt['default']);
+      $("#appDateTime").mobiscroll(optDateTime).datetime(optDateTime);
+      $("#appTime").mobiscroll(optTime).time(optTime);
+
+    //下面注释部分是上面的参数可以替换改变它的样式
+    //希望一起研究插件的朋友加我个人QQ也可以，本人也建个群 291464597 欢迎进群交流。哈哈。这个不能算广告。
+    // 直接写参数方法
+    //$("#scroller").mobiscroll(opt).date();
+    // Shorthand for: $("#scroller").mobiscroll({ preset: 'date' });
+    //具体参数定义如下
+      //{
+      //preset: 'date', //日期类型--datatime --time,
+      //theme: 'ios', //皮肤其他参数【android-ics light】【android-ics】【ios】【jqm】【sense-ui】【sense-ui】【sense-ui】
+                  //【wp light】【wp】
+      //mode: "scroller",//操作方式【scroller】【clickpick】【mixed】
+      //display: 'bubble', //显示方【modal】【inline】【bubble】【top】【bottom】
+      //dateFormat: 'yyyy-mm-dd', // 日期格式
+      //setText: '确定', //确认按钮名称
+      //cancelText: '清空',//取消按钮名籍我
+      //dateOrder: 'yymmdd', //面板中日期排列格
+      //dayText: '日',
+      //monthText: '月',
+      //yearText: '年', //面板中年月日文字
+      //startYear: (new Date()).getFullYear(), //开始年份
+      //endYear: (new Date()).getFullYear() + 9, //结束年份
+      //showNow: true,
+      //nowText: "明天",  //
+      //showOnFocus: false,
+      //height: 45,
+      //width: 90,
+      //rows: 3}
+
+
 });
 
 // 完善个人信息开始
 
 function perfectPersonalInfo(){
     var loginname = $("#loginname").val(); // 用户名
-    var gender = $("input:radio[name='gender']:checked").val(); // 性别
     var birthday = $("#birthday").val(); // 生日
     var email = $("#email").val(); // 邮箱
     var address = $("#address").val(); // 地址
@@ -52,8 +104,8 @@ function perfectPersonalInfo(){
     var idcardFront = $("#idcardFront_").val(); // 身份证正面上传图片的地址
     var idcardBack = $("#idcardBack_").val(); // 身份证反面上传图片的地址
     var banknumber = $("#banknumber").val(); // 银行卡号
-    var alipay = "支付宝";// 支付宝
-    var wechat = "wechat";// 微信
+    var alipay = $("#alipay").val(); // 支付宝
+    var wechat = $("#wechat").val(); // 微信
 
     if("" != phone && !/^1[3,5,7,8,9]\d{9}$/.test(phone) ){
         dialog.alert({
@@ -91,7 +143,6 @@ function perfectPersonalInfo(){
     $.post("http://192.168.0.129:8080/ActorInterface/member/finishInfomation.action",{
         token: localStorage.token,
         loginname: loginname,
-        gender: gender,
         birthday: birthday,
         email: email,
         address: address,
